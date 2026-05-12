b910f3151a00379646b79d2d53f7521065ac237b
Hem Aghara <hemaghara@gmail.com>
2026-05-11 17:16:54 -0700

    commit

--- server/index.js
+++ server/index.js
@@ -5,16 +5,13 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 
-// Basic Route
 app.get('/', (req, res) => {
   res.send('Server is up and running!');
 });
-
-// Start Server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
